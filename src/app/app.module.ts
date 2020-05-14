caa4f6f883a946e645cef86aaaef11f76dd52756
Jesús Rodríguez <jesus.jarp@gmail.com>
2020-05-14 22:09:44 +0200

    separating view into small components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DateComponent } from './date/date.component';
+import { DotComponent } from './dot/dot.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateComponent,
+    DotComponent,
+    ResultsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dot/dot.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dot',
+  templateUrl: './dot.component.html',
+  styleUrls: ['./dot.component.scss']
+})
+export class DotComponent implements OnInit {
+
+  @Input() public number: number;
+  @Input() public extra: boolean;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
